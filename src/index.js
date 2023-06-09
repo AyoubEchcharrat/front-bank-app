5f95b7ec72521c69e700e01b244a0d39b58a81a0
Ayoub Echcharrat <ayoub.ech-charrat@hotmail.fr>
2023-06-09 17:13:58 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import Index from './pages/homepage/Index'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import SignIn from './pages/signin/Signin';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 
 const router = createBrowserRouter(
